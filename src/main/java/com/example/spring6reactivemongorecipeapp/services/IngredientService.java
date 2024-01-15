d77823fbebc07d791f099135fc2a3a33d8130962
Ashish Verma <32788353+ashishmbm23@users.noreply.github.com>
2024-01-15 18:50:26 +0530

    Refactor Ingredient Service to Reactive layer. closes #3

--- src/main/java/com/example/spring6reactivemongorecipeapp/services/IngredientService.java
+++ src/main/java/com/example/spring6reactivemongorecipeapp/services/IngredientService.java
@@ -1,15 +1,16 @@
 package com.example.spring6reactivemongorecipeapp.services;
 
 import com.example.spring6reactivemongorecipeapp.commands.IngredientCommand;
+import reactor.core.publisher.Mono;
 
 /**
  * Created by jt on 6/27/17.
  */
 public interface IngredientService {
 
-    IngredientCommand findByRecipeIdAndIngredientId(String recipeId, String ingredientId);
+    Mono<IngredientCommand> findByRecipeIdAndIngredientId(String recipeId, String ingredientId);
 
-    IngredientCommand saveIngredientCommand(IngredientCommand command);
+    Mono<IngredientCommand> saveIngredientCommand(IngredientCommand command);
 
-    void deleteById(String recipeId, String idToDelete);
+    Mono<Void> deleteById(String recipeId, String idToDelete);
 }
