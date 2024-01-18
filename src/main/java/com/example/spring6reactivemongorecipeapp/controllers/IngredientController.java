ee2388d98fc969673f647eb1d68e850d2c5fc444
Ashish Verma <32788353+ashishmbm23@users.noreply.github.com>
2024-01-18 22:21:30 +0530

    Implemented ingredient validation

--- src/main/java/com/example/spring6reactivemongorecipeapp/controllers/IngredientController.java
+++ src/main/java/com/example/spring6reactivemongorecipeapp/controllers/IngredientController.java
@@ -6,9 +6,11 @@ import com.example.spring6reactivemongorecipeapp.commands.UnitOfMeasureCommand;
 import com.example.spring6reactivemongorecipeapp.services.IngredientService;
 import com.example.spring6reactivemongorecipeapp.services.RecipeService;
 import com.example.spring6reactivemongorecipeapp.services.UnitOfMeasureService;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +23,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class IngredientController {
 
+    public static final String RECIPE_INGREDIENT_INGREDIENTFORM = "recipe/ingredient/ingredientform";
     private final IngredientService ingredientService;
     private final RecipeService recipeService;
     private final UnitOfMeasureService unitOfMeasureService;
@@ -36,7 +39,7 @@ public class IngredientController {
         log.debug("Getting ingredient list for recipe id: " + recipeId);
 
         // use command object to avoid lazy load errors in Thymeleaf.
-        model.addAttribute("recipe", recipeService.findCommandById(recipeId).block());
+        model.addAttribute("recipe", recipeService.findCommandById(recipeId));
 
         return "recipe/ingredient/list";
     }
@@ -44,12 +47,12 @@ public class IngredientController {
     @GetMapping("recipe/{recipeId}/ingredient/{id}/show")
     public String showRecipeIngredient(@PathVariable String recipeId,
                                        @PathVariable String id, Model model){
-        model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(recipeId, id).block());
+        model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(recipeId, id));
         return "recipe/ingredient/show";
     }
 
     @GetMapping("recipe/{recipeId}/ingredient/new")
-    public String newRecipe(@PathVariable String recipeId, Model model){
+    public String newRecipe( @PathVariable String recipeId, Model model){
 
         //make sure we have a good id value
         RecipeCommand recipeCommand = recipeService.findCommandById(recipeId).block();
@@ -62,23 +65,33 @@ public class IngredientController {
         //init uom
         ingredientCommand.setUom(new UnitOfMeasureCommand());
 
-        model.addAttribute("uomList",  unitOfMeasureService.listAllUoms().collectList().block());
+        model.addAttribute("uomList",  unitOfMeasureService.listAllUoms());
 
-        return "recipe/ingredient/ingredientform";
+        return RECIPE_INGREDIENT_INGREDIENTFORM;
     }
 
     @GetMapping("recipe/{recipeId}/ingredient/{id}/update")
     public String updateRecipeIngredient(@PathVariable String recipeId,
                                          @PathVariable String id, Model model){
-        model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(recipeId, id).block());
+        model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(recipeId, id));
 
-        model.addAttribute("uomList", unitOfMeasureService.listAllUoms().collectList().block());
-        return "recipe/ingredient/ingredientform";
+        model.addAttribute("uomList", unitOfMeasureService.listAllUoms());
+        return RECIPE_INGREDIENT_INGREDIENTFORM;
     }
 
     @PostMapping("recipe/{recipeId}/ingredient")
-    public String saveOrUpdate(@ModelAttribute IngredientCommand command){
-        IngredientCommand savedCommand = ingredientService.saveIngredientCommand(command).block();
+    public String saveOrUpdate(@Valid @ModelAttribute("ingredient") IngredientCommand ingredient,
+                               BindingResult result,
+                               Model model){
+        if( result.hasErrors() ){
+            result.getAllErrors().forEach( objectError -> {
+                log.debug( objectError.toString() );
+            });
+            model.addAttribute("uomList", unitOfMeasureService.listAllUoms());
+             return RECIPE_INGREDIENT_INGREDIENTFORM;
+        }
+
+        IngredientCommand savedCommand = ingredientService.saveIngredientCommand(ingredient).block();
 
         log.debug("saved ingredient id:" + savedCommand.getId());
 
